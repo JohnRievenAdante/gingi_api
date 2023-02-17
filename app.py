fcc0db98b1005a8d26318938fc08df233f8640f7
JohnRievenAdante <99161575+JohnRievenAdante@users.noreply.github.com>
2023-02-17 19:36:06 +0800

    flask

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def get_image_category(data:data2):
     return {'transformed': str(predict[0])}
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=4000, debug=True)"""
+    uvicorn.run(app, host='127.0.0.1', port=4000, debug=True)
 
 
 import uvicorn
@@ -80,3 +80,32 @@ def get_image_category(data:data2):
 
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=4000)
+
+"""
+from flask import Flask, request
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+import pickle
+
+app=Flask(__name__)
+
+@app.route('/prediction',methods=['POST'])
+def upload_image():
+    image_bytes=request.data
+    image=np.frombuffer(image_bytes,dtype=np.uint8).reshape((256,256))
+    color_features = image.flatten()
+    # get HOG features from greyscale image
+    # combine color and hog features into a single array
+    flat_features = np.hstack(color_features)
+    ok=flat_features.reshape(1, -1)
+    ss = StandardScaler()
+    bees_stand = ss.fit_transform(ok)
+    gum_pca = ss.fit_transform(bees_stand)
+    loaded_model = pickle.load(open("finalized_model.pkl", 'rb'))
+    predict=loaded_model.predict(gum_pca)
+    return {'transformed': str(predict[0])}
+    #return {'transformed': image.tolist()}
+
+
+if __name__ == '__main__':
+    app.run()
